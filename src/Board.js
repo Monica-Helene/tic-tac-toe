9344e12a4160622264813bb183908424241863df
Monica-helene <mhu1997@hotmail.com>
2024-11-20 15:05:51 +0100

    Board lagt til

---
+++ src/Board.js
@@ -0,0 +1,56 @@
+
+import "./index.css"
+import calculateWinner from "./calculateWinner";
+import Square from "./Square";
+
+function Board({ xIsNext, squares, onPlay, playerNames }) {
+    function handleClick(i) {
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      const nextSquares = squares.slice();
+      if (xIsNext) {
+        nextSquares[i] = 'X';
+      } else {
+        nextSquares[i] = 'O';
+      }
+      onPlay(nextSquares);
+    }
+  
+    const isBoardFull = (board) => {
+      return board.every(cell => cell !== null);
+    };
+  
+    const winner = squares ? calculateWinner(squares) : null; 
+    let status;
+    if (winner) {
+      status = 'Vinner: ' + (winner === 'X' ? playerNames[0] : playerNames[1]);
+    } else if (isBoardFull(squares)) {
+      status = "Uavgjort";
+    } else {
+      status = 'Neste spiller: ' + (xIsNext ? playerNames[0] : playerNames[1]);
+    }
+  
+    return (
+      <>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </>
+    );
+  }
+
+  export default Board;
--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,8 @@ function Header() {
       <header className="header">
         <nav>
           <ul class="buttons">
-            <li><a href='#'>Hjem</a></li>
-            <li><a href='#'>Kontakt</a></li>
+            <li><a href='https://www.google.com/search?q=tic+tac+toe&rlz=1C5CHFA_enNO1126NO1126&oq=tic+tac+toe&gs_lcrp=EgZjaHJvbWUyDggAEEUYJxg5GIAEGIoFMgcIARAuGIAEMgcIAhAAGIAEMgcIAxAAGIAEMgcIBBAAGIAEMgcIBRAAGIAEMgcIBhAAGIAEMgYIBxBFGD3SAQg0ODM3ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8'>Hjem</a></li>
+            <li><a href='mailto:mhu1997@hotmail.no'>Kontakt</a></li>
             <li><a href="https://www.google.com/maps/@35.7040744,139.5577317,3a,75y,289.6h,87.01t,0.72r/data=!3m6!1e1!3m4!1sgT28ssf0BB2LxZ63JNcL1w!2e0!7i13312!8i6656">Ikke sant</a></li>
             <li><a href="https://www.youtube.com/shorts/oQz2tkFhFGk">Nei.</a></li>
           </ul>
