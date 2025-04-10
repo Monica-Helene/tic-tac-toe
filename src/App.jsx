0eb218510710958d65663761af4484e387f9097c
Monica-helene <mhu1997@hotmail.com>
2025-04-10 13:46:36 +0200

    Lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Game from "./Game.jsx";
+import Home from "./Home.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -8,7 +9,7 @@ export default function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<>Hjem</>} />
+        <Route path="/" element={<Home />} />
         <Route path="/spill" element={<Game />} />
         <Route path="/kontakt" element={<>Kontakt</>} />
         <Route path="/om" element={<>Om</>} />
